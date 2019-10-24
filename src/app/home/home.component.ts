fbaf725de795226abbf0a6646ce12ab2a8e17666
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-10-24 17:03:30 -0500

    voted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,16 +9,7 @@ import { AuthService } from '../shared/services/security/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private _route: ActivatedRoute, private _auth: AuthService) {
-    if (_route.queryParams) {
-      _route.queryParams.subscribe(params => {
-        var param = params["user"];
-        if (param) {
-          var user = JSON.parse(param);
-          //_auth.persist(user);
-        }
-      });
-    }
+  constructor() {
 
   }
 
