653d308ef7124db3c4cf5b4b14cbdd849ae1a2d7
Hai Nguyen <nguyenhohai88@gmail.com>
2020-01-22 00:42:10 -0600

    internal navigation

--- src/app/policy/policy.component.ts
+++ src/app/policy/policy.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-policy',
   templateUrl: './policy.component.html',
   styleUrls: ['./policy.component.css']
 })
-export class PolicyComponent implements OnInit {
+export class PolicyComponent implements OnInit, AfterViewChecked {
+  fragment: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
+  }
+
+  ngAfterViewChecked(): void {
+    try {
+      if (this.fragment) {
+        document.querySelector('#' + this.fragment).scrollIntoView();
+      }
+    } catch (e) { }
   }
 
 }
