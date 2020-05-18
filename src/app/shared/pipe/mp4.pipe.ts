cc5cbb5dc2e5d746d0a6618ba53bc1a9a6229f36
Hai Nguyen <nguyenhohai88@gmail.com>
2020-05-17 19:13:19 -0500

    upload and convert

--- src/app/shared/pipe/mp4.pipe.ts
+++ src/app/shared/pipe/mp4.pipe.ts
@@ -5,7 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class Mp4Pipe implements PipeTransform {
     transform(value: string, args: string[]): any {
-        var newValue = value.replace(/\.[^.]+$/, ".mp4");
+        //When video upload on storage there will be another file generated with the same name plus '_output.mp4'
+        var newValue = value.replace(/\.[^.]+$/, "_output.mp4");
         return newValue;
     }
 }
