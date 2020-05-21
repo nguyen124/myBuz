2922781d35bdfee6a600ca3ead8864076f7e63bb
Hai Nguyen <nguyenhohai88@gmail.com>
2020-05-21 13:53:36 -0500

    mp4 thumb

--- src/app/shared/pipe/mp4-not-thumb.pipe.ts
+++ src/app/shared/pipe/mp4-not-thumb.pipe.ts
@@ -5,7 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class Mp4NotThumbPipe implements PipeTransform {
     transform(value: string, args: string[]): any {
-        var newValue = value.replace(/\.[^.]+$/, "_output.mp4");
-        return newValue;
+        const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        if (vw <= 425) {
+            var newValue = value.replace(/\.[^.]+$/, "_output.mp4");
+            return newValue;
+        }
     }
 }
