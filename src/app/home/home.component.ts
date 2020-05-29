d16665a9cd0d71866667379fd91f11e226c73712
Hai Nguyen <nguyenhohai88@gmail.com>
2020-05-29 15:58:40 -0500

    paging

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,11 @@ export class HomeComponent implements OnInit {
   items: IItem[];
   params: any = {};
   nextPage = 0;
-  PER_PAGE = 8;
+  PER_PAGE = 40;
+  MAX_ITEMS = this.PER_PAGE * 4;
+  currentLength: number;
+  offset: number;
+  actualPage: any;
 
   @ViewChild(ItemsComponent, { static: false }) itemsComponent: ItemsComponent
 
@@ -21,7 +25,9 @@ export class HomeComponent implements OnInit {
     this._activatedRoute.queryParams.subscribe(params => {
       if (Object.entries(params).length === 0 && params.constructor === Object) {
         this.params = {
-          temp: "cold"
+          temp: "cold",
+          page: 0,
+          perPage: this.PER_PAGE
         };
         this.getCold();
       } else {
@@ -34,6 +40,9 @@ export class HomeComponent implements OnInit {
   getItems(params) {
     this._itemService.getItems(params).subscribe((newItems: IItem[]) => {
       this.items = newItems;
+      this.currentLength = this.items.length;
+      this.offset = +params.page;
+      this.actualPage = +params.page;
     });
   }
 
@@ -58,21 +67,35 @@ export class HomeComponent implements OnInit {
 
   loadNext() {
     this.params.perPage = this.params.perPage || this.PER_PAGE;
+    if (this.items.length >= this.MAX_ITEMS) {
+      this.offset = Math.floor((this.items.length + this.offset * this.params.perPage) / this.params.perPage);
+      this.items = [];
+      this.nextPage = 0;
+    }
     if (this.items) {
       this.nextPage = Math.floor(this.items.length / this.params.perPage);
     } else {
       this.nextPage = 0;
     }
-    var sending = Object.assign({}, this.params, { page: this.nextPage });
+    this.actualPage = this.offset + this.nextPage;
+    var sending = Object.assign({}, this.params, { page: this.actualPage });
+    console.log("Offset: " + this.offset);
     this._itemService.getItems(sending).subscribe((newItems: IItem[]) => {
       for (var i = 0; i < newItems.length; i++) {
         this.items[this.nextPage * this.params.perPage + i] = newItems[i];
       }
+      this.currentLength = this.items.length;
     });
   }
 
   removeFilter(key) {
     delete this.params[key];
     this._router.navigate(['.'], { queryParams: this.params });
+  }
+
+  loadPage(event) {
+    this.nextPage = +event.target.value;
+    console.log("Load page: " + event.target.value);
+    this.loadNext();
   }
 }
